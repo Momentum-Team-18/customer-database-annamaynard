640b7a6e8269ae7f8704095c617966dc3745a2c2
annamaynard <anna.maynard@gmail.com>
2023-04-25 16:15:27 -0400

    styling & info added

--- main.js
+++ main.js
@@ -1,62 +1,62 @@
-
-let customerProfiles = document.querySelector('#customerProfiles');
+let customerProfiles = document.querySelector("#customerProfiles");
 
 console.log(customerProfiles);
 
-// Build a container for each customer and populate 
+// Build a container for each customer and populate
 
 for (let customer of customers) {
+  
+  let formatedState = nameToAbbr(customer.location.state);
 
-// DIV
-    let customerCard = document.createElement('div');
-    // created the card for this customer
+  // DIV
+  let customerCard = document.createElement("div");
+  // created the card for this customer
 
-// PHOTO/THUMBNAIL
-let customerThumbnail = document.createElement('img');
-customerThumbnail.src = customer.picture.large;
-customerCard.appendChild(customerThumbnail);
+  // CUSTOMERIMAGE
+ 
+  let customerImage = document.createElement("img");
+  customerImage.src = customer.picture.large;
+  customerCard.appendChild(customerImage);
+  customerImage.classList.add('customerImage');
+ 
+ 
+  //  NAME
+  let customerName = document.createElement("h2");
+  customerName.innerText = `${customer.name.first} ${customer.name.last}`;
+  customerCard.appendChild(customerName);
+  customerName.classList.add('customerName');
+  // added the name of the customer to the card
 
-//  NAME 
-    let customerName = document.createElement('h2');
-    customerName.innerText = `${customer.name.first} ${customer.name.last}`;
-    customerCard.appendChild(customerName);
-    // added the name of the customer to the card 
+  // EMAIL
+  let customerEmail = document.createElement("p");
+  customerEmail.innerText = customer.email;
+  customerCard.appendChild(customerEmail);
+  customerEmail.classList.add('customerEmail');
 
-// EMAIL
-    let customerEmail = document.createElement('p');
-    customerEmail.innerText = customer.email;
-    customerCard.appendChild(customerEmail);  
-   
-// PHONE
-    let customerPhone = document.createElement('p');
-    customerPhone.innerText = customer.phone;
-    customerCard.appendChild(customerPhone);
+  // PHONE
+  let customerPhone = document.createElement("p");
+  customerPhone.innerText = customer.phone;
+  customerCard.appendChild(customerPhone);
 
-// ADDRESS
+  // ADDRESS
 
-    let customerAddress = document.createElement('p');
-    customerAddress.innerText = `${customer.location.street.number} ${customer.location.street.name}. ${customer.location.city},  ${customer.location.state} ${customer.location.postcode}`
-    customerCard.appendChild(customerAddress)
+  let customerAddress = document.createElement("p");
+  customerAddress.innerText = `${customer.location.street.number} ${customer.location.street.name}. \n ${customer.location.city},  ${formatedState} ${customer.location.postcode}`;
+  customerCard.appendChild(customerAddress);
 
-// DOB
-    let customerDob = document.createElement('p');
-    customerDob.innerText = `DOB: ${customer.dob.date}`;
-    customerCard.appendChild(customerDob);
-        // something about date formating here 
-    
-// DATE REGISTERED
-    // let customerRegistered = document.createElement('p');
-    // customerRegistered = customer.registered;
-    // customerCard.appendChild(customerRegistered);
+  let formattedDate = moment(customer.registered.date).format("MMM,DD YYYY");
+  let formattedDob = moment(customer.dob.date).format("MMM,DD YYYY");
 
+  // DATES
 
+  let customerDob = document.createElement("p");
+  customerCard.appendChild(customerDob);
 
+  let customerRegistered = document.createElement('p');
+  customerRegistered.innerText = `DOB: ${formattedDob} \n Registered Since: ${formattedDob}`;
+  customerCard.appendChild(customerRegistered);
 
+  customerProfiles.appendChild(customerCard);
 
-    customerProfiles.appendChild(customerCard);
-
-
-
-
-            // creating a div to hold all of each customer info 
+  // creating a div to hold all of each customer info
 }
